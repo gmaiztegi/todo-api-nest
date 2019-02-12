7b24d7e70e139596a390f13e8402d3b222c6eb1b
Gorka Maiztegi Etxeberria <gmaiztegi@gmail.com>
2019-02-12 14:17:31 +0100

    Test the controller

--- src/todo/todo.controller.spec.ts
+++ src/todo/todo.controller.spec.ts
@@ -1,9 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
+import { Todo } from './todo.interface';
 
 describe('Todo Controller', () => {
   let controller: TodoController;
+  let todoService: TodoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,10 +13,20 @@ describe('Todo Controller', () => {
       providers: [TodoService],
     }).compile();
 
+    todoService = module.get<TodoService>(TodoService);
     controller = module.get<TodoController>(TodoController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of todos', () => {
+      const result: Todo[] = [{ _id: 'id1', label: 'todo1' }];
+      jest.spyOn(todoService, 'findAll').mockImplementation(() => result);
+
+      expect(controller.findAll()).toBe(result);
+    });
   });
 });
