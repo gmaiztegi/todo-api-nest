b5491e8792c136d4370c23bfebd6a7976d1bef09
Gorka Maiztegi Etxeberria <gmaiztegi@gmail.com>
2019-02-12 14:06:18 +0100

    Test the service

--- src/todo/todo.service.spec.ts
+++ src/todo/todo.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodoService } from './todo.service';
+import { Todo } from './todo.interface';
 
 describe('TodoService', () => {
   let service: TodoService;
@@ -14,5 +15,35 @@ describe('TodoService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should start empty', () => {
+    expect(service.findAll()).toHaveLength(0);
+  });
+
+  it('should add elements', () => {
+    const todo1 = 'todo1';
+
+    const id = service.add(todo1);
+
+    const todos = service.findAll();
+    expect(todos).toHaveLength(1);
+    expect(todos[0].label).toBe(todo1);
+    expect(todos[0]._id).toBe(id);
+  });
+
+  it('should delete elements', () => {
+    const todo1 = 'todo1';
+    const todo2 = 'todo2';
+
+    const id1 = service.add(todo1);
+    const id2 = service.add(todo2);
+    expect(service.findAll()).toHaveLength(2);
+
+    service.delete(id1);
+
+    const todos = service.findAll();
+    expect(todos).toHaveLength(1);
+    expect(todos[0]._id).toBe(id2);
   });
 });
