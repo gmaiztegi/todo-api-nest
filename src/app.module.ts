999082ed792cc5f95b2d22f346c73a29b21c6682
Gorka Maiztegi Etxeberria <gmaiztegi@gmail.com>
2019-02-15 13:00:12 +0100

    Make port number configurable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from 'nestjs-config';
 import { TodoModule } from './todo/todo.module';
+import { resolve } from 'path';
 
 @Module({
-  imports: [TodoModule],
+  imports: [
+    ConfigModule.load(resolve(__dirname, 'config', '**/!(*.d).{ts,js}')),
+    TodoModule,
+  ],
 })
 export class AppModule {}
