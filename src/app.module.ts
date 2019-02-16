2852944438dfcd082bd17141b54cd3ade00a63ff
Gorka Maiztegi Etxeberria <gmaiztegi@gmail.com>
2019-02-16 16:44:34 +0100

    Add health probe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TerminusModule } from '@nestjs/terminus';
 import { ConfigModule } from 'nestjs-config';
 import { TodoModule } from './todo/todo.module';
 import { resolve } from 'path';
@@ -6,6 +7,9 @@ import { resolve } from 'path';
 @Module({
   imports: [
     ConfigModule.load(resolve(__dirname, 'config', '**/!(*.d).{ts,js}')),
+    TerminusModule.forRoot({
+      endpoints: [{ url: '/health', healthIndicators: [] }],
+    }),
     TodoModule,
   ],
 })
